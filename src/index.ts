b713583c47d7cc9c6f15d12bcb1512589139db66
Waseem Ahmed <codewaseem@gmail.com>
2020-02-05 22:29:26 +0530

    getting youtube search

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,70 @@
-import { observable, computed, action, reaction, when } from "mobx";
+require("dotenv").config();
 
-// configure({
-//   enforceActions: "observed",
-// });
+import { observable, computed, action, runInAction } from "mobx";
 
-interface CartItem {
-  name: string;
-  quantity: number;
-  price: number;
+import { google } from "googleapis";
+
+console.log("API", process.env.YOUTUBE_V3_API);
+
+let youtubeApi = google.youtube({
+  version: "v3",
+  auth: process.env.YOUTUBE_V3_API,
+});
+
+async function searchVideos(searchTerm: string) {
+  let result = await youtubeApi.search
+    .list({
+      part: "id,snippet",
+      q: searchTerm,
+    })
+    .then((r) => r.data);
+  console.log(result);
 }
 
-class Cart {
-  @observable items: CartItem[] = [];
-  @observable modified = Date.now();
-  cancelPriceTracker: Function | null = null;
+enum SearchState {
+  "Empty",
+  "Pending",
+  "Completed",
+  "Failed",
+}
 
-  @computed get description() {
-    switch (this.items.length) {
-      case 0:
-        return `Cart is empty`;
-      case 1:
-        return `Cart only has one item`;
-      default:
-        return `Cart has ${this.items.length} items.`;
-    }
+class BookSearchStore {
+  @observable
+  private term: string = "";
+
+  @observable
+  private status: SearchState = SearchState.Empty;
+
+  @observable.shallow
+  private results: any;
+
+  @action.bound
+  setTerm(term: string) {
+    this.term = term;
+  }
+
+  @computed
+  get totalCount() {
+    return this.results.length;
   }
 
   @action.bound
-  addItem(name: string, price = 500, quantity = 1) {
-    this.items.push({
-      name,
-      price,
-      quantity,
-    });
-  }
-
-  @action.bound
-  changePrice(name: string, newPrice: number) {
-    let item = this.items.find((item) => item.name == name);
-    if (item) {
-      item.price = newPrice;
-    }
-  }
-
-  trackPriceChangeFor(this: Cart, itemName: string) {
-    if (this.cancelPriceTracker) {
-      this.cancelPriceTracker();
-    }
-
-    this.cancelPriceTracker = reaction(
-      () => {
-        const item = this.items.find((item) => item.name == itemName);
-        return item ? item.price : null;
-      },
-      (price) => {
-        console.log(`Price updated for ${itemName}, new price is ${price}`);
-      }
-    );
-  }
-
-  async trackAvailability(this: Cart, itemName: string) {
-    const item = this.items.find((item) => item.name == itemName);
-    await when(() => (item ? item.quantity > 0 : false));
-
-    console.log(`The ${itemName} is now available`);
-  }
-
-  @action.bound
-  changeQuantity(itemName: string, newQuantity: number) {
-    let item = this.items.find((item) => item.name == itemName);
-    if (item) {
-      item.quantity = newQuantity;
+  async search() {
+    try {
+      this.status = SearchState.Pending;
+      const result = await searchVideos(this.term);
+      runInAction(() => {
+        this.results = result;
+        this.status = SearchState.Completed;
+      });
+    } catch (e) {
+      console.log("Error", e);
+      runInAction(() => (this.status = SearchState.Failed));
     }
   }
 }
 
-let cart = new Cart();
+export const bookSearchStore = new BookSearchStore();
 
-cart.addItem("ice cream", 500, 0);
-
-cart.trackPriceChangeFor("ice cream");
-
-cart.changePrice("ice cream", 200);
-cart.changePrice("ice cream", 200);
-cart.changePrice("ice cream", 250);
-cart.trackAvailability("ice cream");
-cart.changeQuantity("ice cream", 4);
-cart.changeQuantity("ice cream", 5);
+bookSearchStore.setTerm("javascript");
+bookSearchStore.search();
