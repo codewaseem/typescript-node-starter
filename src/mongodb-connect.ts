2b9cbcdaf0a9fb80b6484b0c260e253f8d335820
Waseem Ahmed <codewaseem@gmail.com>
2020-02-06 21:05:51 +0530

    working mongodb connection

---
+++ src/mongodb-connect.ts
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+
+const DATABASE_URL = "mongodb://localhost:27017";
+const DATABASE_NAME = "TodoApp";
+
+const mongoClient = new MongoClient(DATABASE_URL, {
+  useUnifiedTopology: true,
+});
+
+mongoClient
+  .connect()
+  .then((client) => {
+    let todoDB = client.db(DATABASE_NAME);
+    console.log(`MongoDB Connected`);
+    client.close();
+  })
+  .catch(() => {
+    console.log(`Failed to connect`);
+  });
