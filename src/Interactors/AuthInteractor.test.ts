90f639a6143cc20dfc2e839288fd4e3982aa273b
Waseem Ahmed <codewaseem@gmail.com>
2020-02-09 11:41:19 +0530

    refactor

--- src/Interactors/AuthInteractor.test.ts
+++ src/Interactors/AuthInteractor.test.ts
@@ -6,6 +6,58 @@ import AuthInteractor, {
   UserID,
 } from "./AuthInteractor";
 
+describe("LoginInteractor", () => {
+  let authInteractor: AuthInteractor;
+
+  beforeEach(() => {
+    authInteractor = new AuthInteractor();
+  });
+
+  test("should have email and password, else throw error", () => {
+    assertAsyncFuncToReject(authInteractor.signup("", "pass"));
+    assertAsyncFuncToReject(authInteractor.signup("email", ""));
+  });
+
+  describe("LoginInteractor with LoginInputValidator", () => {
+    test("should validate email input, if email is bad throws an error", () => {
+      authInteractor.setValidator(new LoginValidatorMock());
+
+      assertAsyncFuncToReject(
+        authInteractor.signup("bad_email", "good_password")
+      );
+      assertAsyncFuncToReject(
+        authInteractor.signup("email_bad", "good_password")
+      );
+    });
+
+    test("should validate password, if password is bad throws an error", () => {
+      authInteractor.setValidator(new LoginValidatorMock());
+      assertAsyncFuncToReject(
+        authInteractor.signup("good@email.com", "bad_pass")
+      );
+    });
+  });
+
+  test("throws an error when user db gateway not set", () => {
+    assertAsyncFuncToReject(
+      authInteractor.signup("good@email.com", "good_pass")
+    );
+  });
+  test("valid input should return new UserID", async () => {
+    authInteractor.setUserGateway(new UserGateWayMockThatSucceeds());
+    let userID = await authInteractor.signup("good@email.com", "good_password");
+
+    expect(userID).toBeInstanceOf(UserID);
+  });
+
+  test("Broken User Gateway should throw an error", () => {
+    authInteractor.setUserGateway(new UserGatewayMockThatBreaks());
+    assertAsyncFuncToReject(
+      authInteractor.signup("good@email.com", "goodPasswerod")
+    );
+  });
+});
+
 class LoginValidatorMock implements LoginInputValidator {
   isValidEmail(email: string): boolean {
     return !email.includes("bad");
@@ -30,58 +82,3 @@ class UserGatewayMockThatBreaks implements UserDBGateway {
 function assertAsyncFuncToReject(funcToCall: Promise<any>) {
   expect(funcToCall).rejects.toBeInstanceOf(Error);
 }
-
-describe("LoginInteractor", () => {
-  let loginInteractor: AuthInteractor;
-
-  beforeEach(() => {
-    loginInteractor = new AuthInteractor();
-  });
-
-  test("should have email and password, else throw error", async () => {
-    assertAsyncFuncToReject(loginInteractor.signup("", "pass"));
-    assertAsyncFuncToReject(loginInteractor.signup("email", ""));
-  });
-
-  describe("LoginInteractor with LoginInputValidator", () => {
-    test("should validate email input, if email is bad throws an error", async () => {
-      loginInteractor.setValidator(new LoginValidatorMock());
-
-      assertAsyncFuncToReject(
-        loginInteractor.signup("bad_email", "good_password")
-      );
-      assertAsyncFuncToReject(
-        loginInteractor.signup("email_bad", "good_password")
-      );
-    });
-
-    test("should validate password, if password is bad throws an error", async () => {
-      loginInteractor.setValidator(new LoginValidatorMock());
-      assertAsyncFuncToReject(
-        loginInteractor.signup("good@email.com", "bad_pass")
-      );
-    });
-  });
-
-  test("throws an error when user db gateway not set", () => {
-    assertAsyncFuncToReject(
-      loginInteractor.signup("good@email.com", "good_pass")
-    );
-  });
-  test("valid input should return new UserID", async () => {
-    loginInteractor.setUserGateway(new UserGateWayMockThatSucceeds());
-    let userID = await loginInteractor.signup(
-      "good@email.com",
-      "good_password"
-    );
-
-    expect(userID).toBeInstanceOf(UserID);
-  });
-
-  test("Broken User Gateway should throw an error", async () => {
-    loginInteractor.setUserGateway(new UserGatewayMockThatBreaks());
-    assertAsyncFuncToReject(
-      loginInteractor.signup("good@email.com", "goodPasswerod")
-    );
-  });
-});
