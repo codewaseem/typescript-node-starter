82c781946953d7a588898517361221ecde518810
Waseem Ahmed <codewaseem@gmail.com>
2020-03-07 15:25:52 +0530

    express app base

---
+++ src/server/app/index.ts
@@ -0,0 +1,33 @@
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import { json, urlencoded } from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+
+const app = express();
+app.disable("x-powered-by");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "*.amazonaws.com"],
+      },
+    })
+  );
+  app.use(compression());
+  app.use(cors());
+}
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+export default app;
