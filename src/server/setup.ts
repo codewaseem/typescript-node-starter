6cd7f667a7d948a59275b66478200b1e097b2f32
Waseem Ahmed <codewaseem@gmail.com>
2020-03-07 17:49:24 +0530

    refactor

--- src/server/setup.ts
+++ src/server/setup.ts
@@ -2,11 +2,15 @@ import app from "./app";
 import gqlServer from "./services/graphql";
 
 export function startServer(port: number = 3000, host: string = "localhost") {
+  addServicesToApp();
   return new Promise((resolve) => {
-    gqlServer.applyMiddleware({ app });
     app.listen(port, host, () => {
       console.log(`Server started at ${host}:${port}`);
       resolve();
     });
   });
+}
+
+function addServicesToApp() {
+  gqlServer.applyMiddleware({ app });
 }
