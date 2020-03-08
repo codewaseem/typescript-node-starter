795e50da233090d090d43cd90502f383432e52fc
Waseem Ahmed <codewaseem@gmail.com>
2020-03-08 17:35:07 +0530

    add typegoose middleware

--- src/server/services/graphql/index.ts
+++ src/server/services/graphql/index.ts
@@ -3,10 +3,12 @@ import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import AuthResolver from "./resolvers/Auth";
+import { TypegooseMiddleware } from "./middlewares/typegoose-middleware";
 
 export default async function getGraphQLServer() {
   const schema = await buildSchema({
     resolvers: [AuthResolver],
+    globalMiddlewares: [TypegooseMiddleware],
   });
   return new ApolloServer({
     schema,
